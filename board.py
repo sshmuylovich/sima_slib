eb7334a1c5c767a7ba7b85b1a54b8b2bd12978be
Sarah Chen <sarahlc888@gmail.com>
2020-10-13 16:35:14 -0400

    debug and restructure board.py

--- board.py
+++ board.py
@@ -1,35 +1,34 @@
 from array import *
 import numpy as np
 
-value = input("Please enter a the letters that appear on your boggle board with spaces in between:\n")
-    
-letters = value.upper()
+def play_game():
 
-def string_to_array(s):
-    return s.split(" ")
+    def one_to_two_array(grid):
+        oneDArray = np.array(grid)
+        twoDArray = oneDArray.reshape(5,5)
 
-grid = string_to_array(letters)
+        return twoDArray
 
-print(f'You entered {value}, as a boggle board it looks like... {letters}')
-print(f'{grid}')
+    def string_to_array(s):
+        return s.split(" ")
 
 
-def one_to_two_array():
-    oneDArray = np.array(grid)
-    twoDArray = oneDArray.reshape(5,5)
+    value = input("Please enter a the 25 letters that appear on your boggle board with spaces in between:\n")
+        
+    letters = value.strip().upper() # remove surrounding whitespace to make this more robust to user variation
 
-    return twoDArray[5][5]
+    grid = one_to_two_array(string_to_array(letters))
 
-print(f'{one_to_two_array()}')
+    print(f'You entered {value}, as a boggle board it looks like...\n{grid}')
 
-# confirm = input("Does this look correct to you? Yes or No\n")
+    # confirm = input("Does this look correct to you? Yes or No\n")
 
-# if confirm.upper() == "YES":
-#     x = 2
-#     break
-# elif confirm.upper() == "NO":
-#     x = 0
-#     print(f'Reinput your letters please.')
-# else:
-#     x = 1
-#     print(f'There was an error, can you please confirm your board again?')
+    # if confirm.upper() == "YES":
+    #     x = 2
+    #     break
+    # elif confirm.upper() == "NO":
+    #     x = 0
+    #     print(f'Reinput your letters please.')
+    # else:
+    #     x = 1
+    #     print(f'There was an error, can you please confirm your board again?')

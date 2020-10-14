87487debf3f8191d8ba78f1a10f16eb5c572a6a4
Sima Shmuylovich <sshmuylovich21@andover.edu>
2020-10-13 21:43:48 -0400

    added code to players.py, I created an array that contains the names of players and an array with the same lengths that acts as a scorecard and is initialized to 0

--- BOGGLE/players.py
+++ BOGGLE/players.py
@@ -1,1 +1,23 @@
+from array import *
+import numpy as np
 
+def get_players_names():
+    value = input("Please enter the name of each players with spaces in between names:\n")
+    
+    names = value.strip().upper()
+    return names
+
+def setup_players_scores():
+    
+    def string_to_array(s):
+        return s.split(" ")
+
+    list_of_names = string_to_array(get_players_names())
+    num_players = len(list_of_names)
+
+    scorecard = np.zeros(num_players)
+
+    print(f'List of players:{list_of_names}\nand their current scores:{scorecard}')
+
+if __name__ == "__main__":
+    setup_players_scores()

bf2f20e6e4411571341622c0e631f1870dea0243
Jeffrey Pan <jeffreyzpan@gmail.com>
2020-10-14 00:31:16 -0500

    added score function and google style docstrings

--- BOGGLE/board.py
+++ BOGGLE/board.py
@@ -2,8 +2,40 @@ from array import *
 import numpy as np
 
 def get_value():
+    '''
+    Function to get the initial state of the board
+    Args:
+        None
+    Returns:
+        value: string of the board state
+    '''
+
         value = input("Please enter the 16 letters that appear on your boggle board with spaces in between:\n")
         return value
+
+def score_word(word):
+    '''
+    Function to score a given Boggle word
+    Args:
+        word: string of the given Boggle word
+    Returns:
+        score: int score
+    '''
+
+    if len(word) < 3:
+        return 1
+    elif len(word) in [3,4]:
+        return 1
+    elif len(word) == 5:
+        return 2
+    elif len(word) == 6:
+        return 3
+    elif len(word) == 7:
+        return 4
+    elif len(word) >= 8:
+        return 11
+    else:
+        return NotImplementedError
 
 def play_game():
 
@@ -17,7 +49,7 @@ def play_game():
 
     def string_to_array(s):
         return s.split(" ")
-    
+
     letters = answer.strip().upper() # remove surrounding whitespace to make this more robust to user variation
 
     if len(string_to_array(letters))!=16:
@@ -25,11 +57,11 @@ def play_game():
         play_game()
         exit()
 
-        
+
     boggle_board = one_to_two_array(string_to_array(letters))
 
     print(f'You entered {answer}, as a boggle board it looks like...\n{boggle_board}')
-    
+
     def confirm_board():
         confirm = input("Does this look like your expected output?\n")
 
@@ -41,7 +73,7 @@ def play_game():
         else:
             print(f'\nThere was an error with your input, try again! This time please respond with yes or no.')
             confirm_board()
-    
+
     confirm_board()
 
 if __name__ == "__main__":

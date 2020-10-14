f965cf04fccb7af2f50dd50446b643305bd47bc2
Sima Shmuylovich <sshmuylovich21@andover.edu>
2020-10-14 09:03:10 -0400

    moved score_word() to score.py, created score.py

--- BOGGLE/board.py
+++ BOGGLE/board.py
@@ -13,30 +13,6 @@ def get_value():
         value = input("Please enter the 16 letters that appear on your boggle board with spaces in between:\n")
         return value
 
-def score_word(word):
-    '''
-    Function to score a given Boggle word
-    Args:
-        word: string of the given Boggle word
-    Returns:
-        score: int score
-    '''
-
-    if len(word) < 3:
-        return 0
-    elif len(word) in [3,4]:
-        return 1
-    elif len(word) == 5:
-        return 2
-    elif len(word) == 6:
-        return 3
-    elif len(word) == 7:
-        return 4
-    elif len(word) >= 8:
-        return 11
-    else:
-        return NotImplementedError
-
 def play_game():
 
     answer = get_value()
---
+++ score.py
@@ -0,0 +1,23 @@
+def score_word(word):
+    '''
+    Function to score a given Boggle word
+    Args:
+        word: string of the given Boggle word
+    Returns:
+        score: int score
+    '''
+
+    if len(word) < 3:
+        return 0
+    elif len(word) in [3,4]:
+        return 1
+    elif len(word) == 5:
+        return 2
+    elif len(word) == 6:
+        return 3
+    elif len(word) == 7:
+        return 4
+    elif len(word) >= 8:
+        return 11
+    else:
+        return NotImplementedError

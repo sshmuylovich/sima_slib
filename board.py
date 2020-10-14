1be35b65a8722042406772f6fa499b0a06588886
Sima Shmuylovich <sshmuylovich21@andover.edu>
2020-10-13 20:51:42 -0400

    finished board.py

--- board.py
+++ board.py
@@ -1,8 +1,14 @@
 from array import *
 import numpy as np
 
+def get_value():
+        value = input("Please enter the 16 letters that appear on your boggle board with spaces in between:\n")
+        return value
+
 def play_game():
-    
+
+    answer = get_value()
+
     def one_to_two_array(grid):
         oneDArray = np.array(grid)
         twoDArray = oneDArray.reshape(4,4)
@@ -11,27 +17,32 @@ def play_game():
 
     def string_to_array(s):
         return s.split(" ")
+    
+    letters = answer.strip().upper() # remove surrounding whitespace to make this more robust to user variation
 
+    if len(string_to_array(letters))!=16:
+        print(f'\nSomething was wrong with your input. Try again!')
+        play_game()
+        exit()
 
-    value = input("Please enter the 16 letters that appear on your boggle board with spaces in between:\n")
         
-    letters = value.strip().upper() # remove surrounding whitespace to make this more robust to user variation
-
     boggle_board = one_to_two_array(string_to_array(letters))
 
-    print(f'You entered {value}, as a boggle board it looks like...\n{boggle_board}')
+    print(f'You entered {answer}, as a boggle board it looks like...\n{boggle_board}')
+    
+    def confirm_board():
+        confirm = input("Does this look like your expected output?\n")
 
-    # confirm = input("Does this look correct to you? Yes or No\n")
-
-    # if confirm.upper() == "YES":
-    #     x = 2
-    #     break
-    # elif confirm.upper() == "NO":
-    #     x = 0
-    #     print(f'Reinput your letters please.')
-    # else:
-    #     x = 1
-    #     print(f'There was an error, can you please confirm your board again?')
+        if confirm.lower() in ['y', 'yes']:
+            print(f'\nGreat!')
+        elif confirm.lower() in ['n', 'no']:
+            print(f'\nTry again!')
+            play_game()
+        else:
+            print(f'\nThere was an error with your input, try again! This time please respond with yes or no.')
+            confirm_board()
+    
+    confirm_board()
 
 if __name__ == "__main__":
     play_game()
